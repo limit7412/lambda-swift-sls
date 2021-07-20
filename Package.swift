443c3d166c047b0bffcd57f787addea524b3b549
qazx7412 <dev@newgenerations.im>
2021-07-20 19:08:24 +0900

    change dir

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "lambda-swift-sls",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "lambda-swift-sls",
-            targets: ["lambda-swift-sls"]),
-    ],
+    // products: [
+    //     // Products define the executables and libraries a package produces, and make them visible to other packages.
+    //     .library(
+    //         name: "lambda-swift-sls",
+    //         targets: ["lambda-swift-sls"]),
+    // ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
