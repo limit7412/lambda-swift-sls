8a14587e2819056b41a917a51ed44db557438daf
kairox <git@oxymoron.link>
2024-06-12 15:34:04 +0900

    setup json serialize

--- Sources/LambdaSwiftServerless/Main.swift
+++ Sources/LambdaSwiftServerless/Main.swift
@@ -1,17 +1,47 @@
+import Foundation
+
+struct HelloResponse: Codable {
+  let msg: String
+}
+
+struct WorldResponse: Codable {
+  let msg: String
+  let body: String
+}
+
 @main
 struct Main {
   static func main() async throws {
     let _ = try await Serverless.Lambda
       .Handler(
         name: "hello",
-        callback: { event in
-          "{\"statusCode\": 200, \"body\": \"財布ないわ\"}"
+        callback: { (event: Serverless.LambdaAPIGatewayRequest) -> Serverless.LambdaResponse in
+          let body = try Serverless.Json.Encoder(
+            input: HelloResponse(
+              msg: "財布ないわ"
+            )
+          )
+
+          return Serverless.LambdaResponse(
+            statusCode: 200,
+            body: body
+          )
         }
       )
       .Handler(
         name: "world",
-        callback: { event in
-          "{\"statusCode\": 200, \"body\": \"財布ないわ  \(event)\"}"
+        callback: { (event: Serverless.LambdaAPIGatewayRequest) -> Serverless.LambdaResponse in
+          let body = try Serverless.Json.Encoder(
+            input: WorldResponse(
+              msg: "財布ないわ",
+              body: event.body!
+            )
+          )
+
+          return Serverless.LambdaResponse(
+            statusCode: 200,
+            body: body
+          )
         }
       )
   }
--- Sources/LambdaSwiftServerless/Runtime/Serverless.swift
+++ Sources/LambdaSwiftServerless/Runtime/Serverless.swift
@@ -4,8 +4,40 @@ import NIOCore
 import NIOFoundationCompat
 
 struct Serverless {
+  struct LambdaAPIGatewayRequest: Codable {
+    let body: String?
+  }
+
+  struct LambdaResponse: Codable {
+    let statusCode: Int
+    let body: String
+  }
+
+  struct ErrorResponse: Codable {
+    let msg: String
+    let err: String
+  }
+
+  struct Json {
+    static func Encoder<T: Encodable>(input: T) throws -> String {
+      let encoder = JSONEncoder()
+      let data = try encoder.encode(input)
+
+      return String(data: data, encoding: .utf8)!
+    }
+    static func Decoder<T: Decodable>(json: String) throws -> T {
+      let decoder = JSONDecoder()
+      guard let data = json.data(using: .utf8) else {
+        throw NSError(domain: "Invalid JSON format", code: 0, userInfo: nil)
+      }
+      return try decoder.decode(T.self, from: data)
+    }
+  }
   struct Lambda {
-    static func Handler(name: String, callback: (String) async throws -> String) async throws
+    static func Handler<T: Decodable>(
+      name: String, callback: (T) async throws -> LambdaResponse
+    )
+      async throws
       -> Lambda.Type
     {
       if name != ProcessInfo.processInfo.environment["_HANDLER"]! {
@@ -22,23 +54,37 @@ struct Serverless {
         do {
           var byteBody = try await response.body.collect(upTo: 1024 * 1024)
           let responseData = byteBody.readData(length: byteBody.readableBytes) ?? Data()
-          let body = String(data: responseData, encoding: .utf8)!
+          let stringBody = String(data: responseData, encoding: .utf8)!
+          let body: T = try Json.Decoder(json: stringBody)
 
           let result = try await callback(body)
+          let resBody = try Json.Encoder(input: result)
 
           var request = HTTPClientRequest(
             url: "http://\(api)/2018-06-01/runtime/invocation/\(request_id)/response")
           request.method = .POST
-          request.body = .bytes(ByteBuffer(string: result))
+          request.body = .bytes(ByteBuffer(string: resBody))
           let _ = try await HTTPClient.shared.execute(request, timeout: .seconds(30))
         } catch {
           print(error)
 
+          let errBody = try Json.Encoder(
+            input: ErrorResponse(
+              msg: "Internal Lambda Error",
+              err: error.localizedDescription
+            )
+          )
+          let resBody = try Json.Encoder(
+            input: LambdaResponse(
+              statusCode: 500,
+              body: errBody
+            )
+          )
+
           var request = HTTPClientRequest(
             url: "http://\(api)/2018-06-01/runtime/invocation/\(request_id)/error")
           request.method = .POST
-          request.body = .bytes(
-            ByteBuffer(string: "{\"statusCode\": 500, \"body\": \"Internal Lambda Error\"}"))
+          request.body = .bytes(ByteBuffer(string: resBody))
           let _ = try await HTTPClient.shared.execute(request, timeout: .seconds(30))
         }
       }
